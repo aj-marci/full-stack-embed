dddad95f2206928b71cb04650052926b88363df7
AJ Marcinek <abmarcinek@gmail.com>
2024-06-05 13:51:33 -0500

    test again

--- src/SHRcustomTest.js
+++ src/SHRcustomTest.js
@@ -16,25 +16,3 @@ unlayer.registerPropertyEditor({
       },
     }),
   });
-
-  unlayer.ready(function() {
-    var stockTextTool = unlayer.getTool('text');
-    
-    // Modify the tool's options to add the custom property editor
-    stockTextTool.options.default = stockTextTool.options.default || {};
-    stockTextTool.options.default.title = stockTextTool.options.default.title || null;
-  
-    stockTextTool.options.text = stockTextTool.options.text || {};
-    stockTextTool.options.text.title = stockTextTool.options.text.title || 'Text';
-    stockTextTool.options.text.position = stockTextTool.options.text.position || 1;
-  
-    stockTextTool.options.text.options = stockTextTool.options.text.options || {};
-    stockTextTool.options.text.options.letterSpacing = {
-      label: 'Letter Spacing',
-      defaultValue: '0px',
-      widget: 'my_letter_spacing_picker', // custom property editor for letter spacing
-    };
-  
-    // Re-register the modified tool
-    unlayer.registerTool(stockTextTool);
-  });
