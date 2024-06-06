eaddd6edf69964d9cff0d1714ea85c814ca706f4
AJ Marcinek <abmarcinek@gmail.com>
2024-06-06 14:11:49 -0500

    removed font weight from prop editor

--- src/SHRCustom.js
+++ src/SHRCustom.js
@@ -45,47 +45,22 @@ unlayer.registerTool({
 
 
   unlayer.registerPropertyEditor({
-    name: 'my_font_and_spacing_picker',
+    name: 'my_letter_spacing_picker',
     layout: 'bottom',
     Widget: unlayer.createWidget({
       render(value, updateValue, data) {
-        const letterSpacing = value.letterSpacing ? value.letterSpacing.replace('px', '') : '0';
-        const fontWeight = value.fontWeight || '400';
         return `
-          <div>
-            <label>Letter Spacing: 
-              <input class="letter-spacing" type="number" value="${letterSpacing}" step="0.1" /> px
-            </label>
-          </div>
-          <div>
-            <label>Font Weight: 
-              <input class="font-weight" type="number" value="${fontWeight}" min="100" max="900" step="100" />
-            </label>
-          </div>`;
+          <input class="value" type="number" value="${value.replace('px', '')}" step="0.1" /> px`;
       },
       mount(node, value, updateValue, data) {
-        const letterSpacingInput = node.querySelector('.letter-spacing');
-        const fontWeightInput = node.querySelector('.font-weight');
-  
-        letterSpacingInput.onchange = function (e) {
-          const newValue = {
-            ...value,
-            letterSpacing: e.target.value + 'px'
-          };
-          updateValue(newValue);
-        };
-  
-        fontWeightInput.onchange = function (e) {
-          const newValue = {
-            ...value,
-            fontWeight: e.target.value
-          };
-          updateValue(newValue);
+        var input = node.querySelector('.value');
+
+        input.onchange = function (e) {
+          updateValue(e.target.value + 'px');
         };
       },
     }),
   });
-  
 
 
   
